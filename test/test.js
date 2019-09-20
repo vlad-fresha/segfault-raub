53fd10c67f2658801094da7eb7f10ec12ecc1ad6
Luis Blanco <luisblanco1337@gmail.com>
2019-09-20 16:28:41 +0300

    Fix test

--- test/test.js
+++ test/test.js
@@ -11,10 +11,6 @@ describe('Segfault', () => {
 		expect(Segfault).to.be.an('object');
 	});
 	
-	it('contains `registerHandler` function', () => {
-		expect(Segfault.registerHandler).to.be.a('function');
-	});
-	
 	it('contains `causeSegfault` function', () => {
 		expect(Segfault.causeSegfault).to.be.a('function');
 	});
