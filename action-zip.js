534c204adc93adddb15964d6ce67dbd3b93ed855
Luis Blanco <luisblanco1337@gmail.com>
2023-01-03 12:37:52 +0400

    Add zip log

--- action-zip.js
+++ action-zip.js
@@ -3,16 +3,16 @@
 const util = require('node:util');
 const exec = util.promisify(require('node:child_process').exec);
 
-const { platform, bin } = require('addon-tools-raub');
+const { bin } = require('addon-tools-raub');
 
 
 (async () => {
 	try {
-		if (platform === 'windows') {
-			await exec(`powershell Compress-Archive -Path ./${bin}/* -DestinationPath ./${bin}.zip`);
-		} else {
-			await exec(`cd ${bin} && tar -acf ../${bin}.zip *`);
-		}
+		// if (platform === 'windows') {
+		// 	await exec(`powershell Compress-Archive -Path ./${bin}/* -DestinationPath ./${bin}.zip`);
+		// } else {
+		await exec(`cd ${bin} && tar -acf ../${bin}.zip *`);
+		// }
 		
 		console.log(`zip=${bin}.zip`);
 	} catch (error) {
