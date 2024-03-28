dc73a572ad938d58732beac931f8e0a2ab5874eb
Enrique Pessôa <67805781+epessoa-i@users.noreply.github.com>
2024-03-28 12:11:14 -0300

    add TypeScript definition for all the exported functions

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,10 @@ declare module "segfault-raub" {
 	 * Issue an actual segfault, accessing some unavailable memory
 	*/
 	export const causeSegfault: () => void;
+	export const causeDivisionInt: () => void;
+	export const causeOverflow: () => void;
+	export const causeIllegal: () => void;
+
+	// enable / disable signal handlers
+	export const setSignal: (signalId: number, value: boolean) => void;
 }
